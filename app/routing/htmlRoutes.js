674be3d2f9cb99d4f64912460a925972de86d801
sal48 <sal.salazar1@gmail.com>
2017-08-20 01:42:41 -0700

    adds genius questionnaire

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,6 +5,10 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
 
+  app.get("/genius-questionnaire", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/genius-questionnaire.html"));
+  });
+
   // If no matching route is found default to home
   app.use(function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/home.html"));
